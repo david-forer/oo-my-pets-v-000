1f25e347639efd6423682188a2e6c07ed46ed358
David Forer <david.j.forer@gmail.com>
2019-03-10 09:15:23 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -36,15 +36,43 @@ class Owner
     "I am a #{@species}."
   end
 
-  def pets
-    @pets
-  end
+  
+  def buy_fish(fish_name)
+   fish = Fish.new(fish_name)
+   @pets[:fishes] << fish
+ end
 
-   def buy_fish(fish)
-   
-    @pets[:fishes].each do |fish|
-      fish == fish
-      fish
-    end
-  end
-end
+  def buy_cat(cat_name)
+   cat = Cat.new(cat_name)
+   @pets[:cats] << cat
+ end
+
+  def buy_dog(dog_name)
+   dog = Dog.new(dog_name)
+   @pets[:dogs] << dog
+ end
+
+  def walk_dogs
+   @pets[:dogs].each {|dog| dog.mood = "happy"}
+ end
+
+  def play_with_cats
+   @pets[:cats].each {|cat| cat.mood = "happy"}
+ end
+
+  def feed_fish
+   @pets[:fishes].each {|fish| fish.mood = "happy"}
+ end
+
+  def sell_pets
+   @pets.each {|key, array_of_pets| array_of_pets.each {|pet| pet.mood = "nervous"}}
+   @pets = {
+     fishes: [],
+     cats: [],
+     dogs: []
+   }
+   end
+
+    def list_pets
+     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+   end
